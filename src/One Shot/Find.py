daa99175a792d14ae90cb38d5a17c0470915cdbe
Shubhankar Mahajan <ssmahajan01@gmail.com>
2022-01-27 00:38:04 +0530

    Made a few more changes

--- src/One Shot/Find.py
+++ src/One Shot/Find.py
@@ -13,6 +13,7 @@ from keras.applications.vgg16 import preprocess_input
 from keras.layers import Dropout, Flatten, Dense
 from keras.models import Model, Sequential
 from keras.layers import Dense, GlobalAveragePooling2D
+from keras.models import model_from_json
 import os,cv2,pickle
 def get_hog_vec(img,name):
     resized_img = resize(img, (128*4, 64*4))
@@ -44,29 +45,36 @@ def lbp_calculated_pixel(img, x, y):
 	return val
 
 #---------------------------------<><><><><><><>---------------------------------
-model = Sequential()
-model.add(Conv2D(64, kernel_size=(3, 3), input_shape=(240, 320, 3), padding='VALID'))
-model.add(Conv2D(64, kernel_size=(3, 3), padding='VALID'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+# model = Sequential()
+# model.add(Conv2D(64, kernel_size=(3, 3), input_shape=(240, 320, 3), padding='VALID'))
+# model.add(Conv2D(64, kernel_size=(3, 3), padding='VALID'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# Block 2
-model.add(Conv2D(128, kernel_size=(3, 3), strides=1, activation='relu', padding='VALID'))
-model.add(Conv2D(128, kernel_size=(3, 3), strides=1, activation='relu', padding='VALID'))
-model.add(AveragePooling2D(pool_size=(19, 19)))
+# # Block 2
+# model.add(Conv2D(128, kernel_size=(3, 3), strides=1, activation='relu', padding='VALID'))
+# model.add(Conv2D(128, kernel_size=(3, 3), strides=1, activation='relu', padding='VALID'))
+# model.add(AveragePooling2D(pool_size=(19, 19)))
 
-# set of FC => RELU layers
+# # set of FC => RELU layers
+# os.chdir("../One Shot")
+# # print(os.getcwd())
+# model.add(Flatten())
+# model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+# model.save_weights('./models/model_weights.h5')
+# model_json = model.to_json()
+# with open("./models/model.json", "w") as json_file:
+#     json_file.write(model_json)
+# json_file.close()    
+# f = open('./models/history.pckl', 'wb')
+# pickle.dump(model.history, f)
+# f.close()
 os.chdir("../One Shot")
-# print(os.getcwd())
-model.add(Flatten())
-model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-model.save_weights('./models/model_weights.h5')
-model_json = model.to_json()
-with open("./models/model.json", "w") as json_file:
-    json_file.write(model_json)
+with open('models/model.json', "r") as json_file:
+    loaded_model_json = json_file.read()
+    model = model_from_json(loaded_model_json)
 json_file.close()    
-f = open('./models/history.pckl', 'wb')
-pickle.dump(model.history, f)
-f.close()
+model.load_weights("models/model_weights.h5")
+model.make_predict_function()
 acc = 1
 final_predictions = []
 testing_img = sys.argv[1]
